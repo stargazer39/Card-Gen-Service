6c419369d8531bca8f8c35fdc18a63e9687fa9f0
StargazeR Weerakkody <dehemiweerakkodi2@gmail.com>
2022-06-01 22:09:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+app.set('view engine', 'pug')
+app.use(express.static('images'))
+
+app.post("/ticket", upload.any(), (req, res) => {
+    try{
+        const id = uuidv4();
+
+        console.log('POST /post_pdf/');
+        console.log('Files: ', req.files);
+
+        if(req.files.length < 1){
+            res.sendStatus(400);
+            return
+        }
+
+        fs.writeFile(`images/${id}.png`, req.files[0].buffer, (err) => {
+            if (err) {
+                console.log('Error: ', err);
+                res.status(500).send('An error occurred: ' + err.message);
+            } else {
+                res.status(200).json({ id: id });
+            }
+        });
+    }catch(err){
+        console.log('Error: ', err);
+        res.status(500).send('An error occurred: ' + err.message);
+    }
+});
+
+app.get("/ticket/:id", (req, res) => {
+    try{
+        const { id } = req.params;
+
+        res.render('metadata', { image_src: `${req.hostname}/ticket/${id}/image` });
+        console.log(id);
+    }catch(err){
+        console.log('Error: ', err);
+        res.status(500).send('An error occurred: ' + err.message);
+    } 
+});
+
+app.get("/ticket/:id/image", (req, res) => {
+    try{
+        const { id } = req.params;
+
+        res.sendFile(path.resolve(`images/${id}.png`));
+        console.log(id);
+    }catch(err){
+        console.log('Error: ', err);
+        res.status(500).send('An error occurred: ' + err.message);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on - ${PORT}`)
+});
